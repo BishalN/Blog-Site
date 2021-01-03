c6e78b67b4bfec42752948325f1d450996c2fbd5
Bishal Neupane <neupanebishal07@gmail.com>
2021-01-03 07:41:02 +0545

    Ready for deployment

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.class}>
+      <h1>Hello i'm the header component using the sass for styles</h1>
+    </div>
+  );
+};
+
+export default Header;
